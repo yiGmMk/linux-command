c99720ad34d6cce8d1bb630fd313da7835b70f4e
yiGmMk <marvelousme@163.com>
2025-10-20 03:18:53 +0000

    add sitemap

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 const SitemapGenerator = require('sitemap-generator');
 
 const xmlFile = path.join(process.cwd(), '.deploy/sitemap.xml')
-const generator = SitemapGenerator('http://192.168.31.69:1987', {
+const generator = SitemapGenerator('https://linux.programnotes.cn', {
   maxDepth: 0,
   filepath: xmlFile,
   maxEntriesPerFile: 50000,
@@ -14,7 +14,7 @@ const generator = SitemapGenerator('http://192.168.31.69:1987', {
 // register event listeners
 generator.on('done', async () => {
   const str = await fs.readFileSync(xmlFile);
-  const newStr = str.toString().replace(/http\:\/\/192\.168\.31\.69\:1987/g, 'https://wangchujiang.com/linux-command');
+  const newStr = str.toString();
   await fs.outputFile(xmlFile, newStr);
   console.log('sitemaps created!')
 });
